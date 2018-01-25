e39b5b40ec935938caac04108014a5ccae1a6b17
Shawn T <shawn@huthunt.com>
2018-01-25 16:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Code your solution in this file!
-
 const returnFirstTwoDrivers = function (array) {
   return newArr = [array[0], array[1]]
 };
 
-const returnLastTwoDrivers = function (array) {
-  return newArr2 = [array[2], array[3]]
+const returnLastTwoDrivers = function () {
+  return 6 + 9;
 };
-
-selectingDrivers = [ (returnFirstTwoDrivers(), returnLastTwoDrivers())]
